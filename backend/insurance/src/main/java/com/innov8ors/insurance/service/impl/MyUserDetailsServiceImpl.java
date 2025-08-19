d57c57db0b58a32fcb88e94cc6188d96754815a5
sharathtn <sharathtn@zeta.tech>
2025-08-19 10:25:39 +0530

    Add logging to controllers and services for better traceability

--- backend/insurance/src/main/java/com/innov8ors/insurance/service/impl/MyUserDetailsServiceImpl.java
+++ backend/insurance/src/main/java/com/innov8ors/insurance/service/impl/MyUserDetailsServiceImpl.java
@@ -3,12 +3,14 @@ package com.innov8ors.insurance.service.impl;
 import com.innov8ors.insurance.entity.User;
 import com.innov8ors.insurance.entity.UserPrincipal;
 import com.innov8ors.insurance.repository.dao.UserDao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class MyUserDetailsServiceImpl implements UserDetailsService {
 
     private final UserDao userDao;
@@ -19,6 +21,7 @@ public class MyUserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.debug("Loading user by email: {}", email);
         User user = userDao.getByEmail(email).orElseThrow();
         return new UserPrincipal(user);
     }
