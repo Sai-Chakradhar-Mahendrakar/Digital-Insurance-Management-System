5ed548116e0e91ee041b6146ba26f0dfd8ab84f2
sharathtn <sharathtn@zeta.tech>
2025-08-19 11:57:29 +0530

    Refactor user registration to include token in response. Add token in register response itself

--- backend/insurance/src/main/java/com/innov8ors/insurance/mapper/UserMapper.java
+++ backend/insurance/src/main/java/com/innov8ors/insurance/mapper/UserMapper.java
@@ -3,15 +3,17 @@ package com.innov8ors.insurance.mapper;
 import com.innov8ors.insurance.entity.User;
 import com.innov8ors.insurance.request.UserCreateRequest;
 import com.innov8ors.insurance.response.LoginResponse;
-import com.innov8ors.insurance.response.UserCreateResponse;
+import com.innov8ors.insurance.response.UserRegisterResponse;
 
 import static com.innov8ors.insurance.util.InsuranceUtil.MAPPER;
 import static com.innov8ors.insurance.util.InsuranceUtil.encodePassword;
 
 public class UserMapper {
 
-    public static UserCreateResponse getRegisterResponseFromUser(User user) {
-        return MAPPER.map(user, UserCreateResponse.class);
+    public static UserRegisterResponse getRegisterResponseFromUser(User user, String token) {
+        UserRegisterResponse userRegisterResponse = MAPPER.map(user, UserRegisterResponse.class);
+        userRegisterResponse.setToken(token);
+        return userRegisterResponse;
     }
 
     public static LoginResponse getLoginResponseFromToken(String token) {
