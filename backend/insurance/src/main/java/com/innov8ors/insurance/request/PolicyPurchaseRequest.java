d56b96e04cb4809dcb33e759c4a0c6d5374444f7
Arya <makadiaaryam@zeta.tech>
2025-08-21 10:11:06 +0000

    added the validation

--- backend/insurance/src/main/java/com/innov8ors/insurance/request/PolicyPurchaseRequest.java
+++ backend/insurance/src/main/java/com/innov8ors/insurance/request/PolicyPurchaseRequest.java
@@ -1,5 +1,6 @@
 package com.innov8ors.insurance.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 
 @Data
 public class PolicyPurchaseRequest {
+    @NotNull
     private Long policyId;
     private BigDecimal premiumPaid;
 
