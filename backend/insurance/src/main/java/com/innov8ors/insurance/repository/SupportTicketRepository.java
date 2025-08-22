5ad53f3022f043a47065d775908da79e7ede8fb7
Sai Teja Ambati <saitejaa@zeta.tech>
2025-08-22 10:51:24 +0530

    add repository annotation

--- backend/insurance/src/main/java/com/innov8ors/insurance/repository/SupportTicketRepository.java
+++ backend/insurance/src/main/java/com/innov8ors/insurance/repository/SupportTicketRepository.java
@@ -1,9 +1,10 @@
 package com.innov8ors.insurance.repository;
 
 import com.innov8ors.insurance.entity.SupportTicket;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface SupportTicketRepository extends BaseRepository<SupportTicket, Long> {
     List<SupportTicket> findByUserId(Long userId);
 }
